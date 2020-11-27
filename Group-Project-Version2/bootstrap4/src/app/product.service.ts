3b213cb7dff70ef8dc340dc836405bf430fa8233
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-11-26 19:59:48 -0500

    Added web service

--- Group-Project-Version2/bootstrap4/src/app/product.service.ts
+++ Group-Project-Version2/bootstrap4/src/app/product.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Product } from './model/product';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
 
-  constructor() { }
+export class ProductService {
+  baseUrl: string = environment.baseUrl;
+  
+  constructor(private httpClient: HttpClient) { }
+  
+  public getProducts() {
+    return this.httpClient.get(`${this.baseUrl+ 'products'}`);
+  }
 }
--- Group-Project-Version2/bootstrap4/src/app/search-product/search-product.component.ts
+++ Group-Project-Version2/bootstrap4/src/app/search-product/search-product.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../product.service'
 
 @Component({
   selector: 'app-search-product',
   templateUrl: './search-product.component.html',
   styleUrls: ['./search-product.component.css']
 })
+
 export class SearchProductComponent implements OnInit {
   productId: number;
   productDescriptionEnglish: string;
+  message: string;
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
   }
 
+  search(){
+    this.productService.getProducts().subscribe((res:any)=>{
+      console.log(res);
+    },(err:any) => {
+      console.log(err.error.status)
+    })
+  }
 }
